0ad2131d15bd6443a65d30da1bd70c9bcabecb9d
M.Aoyama <maruuca.com@gmail.com>
2023-12-06 22:41:53 +0900

    7章まで

--- script.js
+++ script.js
@@ -15,4 +15,47 @@ $(function () {
       marginLeft: 0
     }, 100);
   });
+
+// カルーセルの設定
+  $('.carousel').slick({
+    autoplay: true,
+    dots: true,
+    infinite: true,
+    autoplaySpeed: 5000,
+    arrows: false,
+  });
+
+  // AjaxでSTATIC FORMにデータを送信
+  $('#submit').on('click', function (event) {
+    // formタグによる送信を拒否
+    event.preventDefault();
+
+    // 入力チェックをした結果、エラーがあるかないか判定
+    let result = inputCheck();
+  });
+
+  // フォーカスが外れた時(blur)にフォームの入力チェックをする
+  $('#name').blur(function () {
+    inputCheck();
+  });
+  $('#furigana').blur(function () {
+    inputCheck();
+  });
+  $('#email').blur(function () {
+    inputCheck();
+  });
+  $('#tel').blur(function () {
+    inputCheck();
+  });
+  $('#message').blur(function () {
+    inputCheck();
+  });
+  $('#agree').blur(function () {
+    inputCheck();
+  });  
+
+  // お問い合わせフォームの入力チェック
+  function inputCheck() {
+    console.log('inputoCheck関数の呼び出し');
+  }
 });
